42f21071129adf0d62141567a37aaaf35c1fa533
juampa95 <jpmanzano95@gmail.com>
2024-01-30 18:06:00 -0300

    agregado build_remote para GitHub Actions

--- build_remote.sh
+++ build_remote.sh
@@ -2,9 +2,9 @@
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+rm -rf public
 reflex init
 API_URL=https://clock-app.up.railway.app reflex export --frontend-only
-rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
